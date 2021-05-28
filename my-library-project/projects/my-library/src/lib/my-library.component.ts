bd4e12e4df4ea2872097966798740843ec0495fd
Dan Radu <dan.radu@msg.group>
2021-05-28 16:34:28 +0300

    initial

---
+++ my-library-project/projects/my-library/src/lib/my-library.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-library',
+  templateUrl: 'my-library.component.html',
+  styleUrls: ['./my-library.component.scss']
+
+})
+export class MyLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
