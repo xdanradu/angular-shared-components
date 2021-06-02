bab222944bc9da4475745dc8e0c67b37ad231b7a
Dan Radu <dan.radu@msg.group>
2021-06-02 08:47:07 +0300

    setup

--- my-library-project/projects/my-library/src/lib/my-library.component.ts
+++ my-library-project/projects/my-library/src/lib/my-library.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-library',
-  templateUrl: 'my-library.component.html',
-  styleUrls: ['./my-library.component.scss']
+  templateUrl: 'my-library.component.html'
 
 })
 export class MyLibraryComponent implements OnInit {
